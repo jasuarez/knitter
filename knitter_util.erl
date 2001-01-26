b08759ef3d74ae9a4fcb1671b1f10844f5a250b8
Juan A. Suarez Romero <jsuarezr@udc.es>
2001-01-26 12:42:39 +0000

    - La función "keyssearch" devuelve valores similares a la correspondiente de "lists"
    git-svn-id: file:///var/svn/repos/knitter/trunk@16 55580ce4-9f14-0410-99c4-eacaa80e0a6d

--- knitter_util.erl
+++ knitter_util.erl
@@ -2,9 +2,9 @@
 -author('$Author$').
 -vsn('$Revision$').
 
-
-
 -export([keysdelete/3, keyssearch/3]).
+
+
 
 keysdelete(Key, N, TupleList) ->
     keysdelete(Key, N, TupleList, []).
@@ -33,47 +33,9 @@ keyssearch(Key, N, [H | T], Accum) ->
 	    keyssearch(Key, N, T, Accum)
     end;
 keyssearch(_, _, [], Accum) ->
-    Accum.
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%-export([start/0, server/1]).
-%-export([getParam/2, delParam/2, setParam/3]).
-%
-%
-%start () ->
-%    spawn(knitter_util, server, [1]).
-%
-%
-%server (NumID) ->
-%    receive
-%	{From, newID} ->
-%	    From ! {newID, NumID},
-%	    server(NumID + 1);
-%	_ ->
-%	    server(NumID)
-%    end.
-%
-%
-%getParam([{Param, Value} | _], Param) ->
-%    {ok, Value};
-%getParam([_ | T], Param) ->
-%    getParam(T, Param);
-%getParam([], _) ->
-%    {error, "Undefined parameter"}.
-%
-%
-%delParam(KQMLMesg, Param) ->
-%    delParam(KQMLMesg, Param, []).
-%
-%
-%delParam([{Param, _} | T], Param, L) ->
-%    {ok, T ++ L};
-%delParam([H | T], Param, L) ->
-%    delParam(T, Param, [H | L]);
-%delParam([], _, L) ->
-%    {undef, L}.
-%
-%
-%setParam(KQMLMesg, Param, Value) ->
-%    {_, K} = delParam(KQMLMesg, Param),
-%    {ok, [{Param, Value} | K]}.
+    case Accum of
+	[] ->
+	    false;
+	_ ->
+	    {values, Accum}
+    end.
