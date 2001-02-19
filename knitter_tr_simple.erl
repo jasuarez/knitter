e85421636ca97c4e65465fdcd96537d72ecccb6f
Juan A. Suarez Romero <jsuarezr@udc.es>
2001-02-19 19:05:30 +0000

    - Enlazados los procesos. - Cambiado el nombre del proceso conversor de mensajes. - Añadido un control de errores del proceso conversor de mensajes.
    git-svn-id: file:///var/svn/repos/knitter/trunk@41 55580ce4-9f14-0410-99c4-eacaa80e0a6d

--- knitter_tr_simple.erl
+++ knitter_tr_simple.erl
@@ -4,7 +4,7 @@
 -vsn('$Revision$ ').
 
 -export([start/2]).
--export([server/2, listenIncoming/2, processMesg/1, attend/2]).
+-export([server/2, listenIncoming/2, server_mesg_conv/1, attend/2]).
 
 
 
@@ -20,32 +20,38 @@ start(Control, Agent) ->
 server(Control, Options) ->
     {value, {port, Port}} = lists:keysearch(port, 1, Options),
     {ok, ListenSocket} = gen_tcp:listen(Port, []),
-    spawn(knitter_tr_simple, listenIncoming, [self(), ListenSocket]),
-    ProcessPID = spawn(knitter_tr_simple, processMesg, [self()]),
-    server(Control, ProcessPID, []).
+    spawn_link(knitter_tr_simple, listenIncoming, [self(), ListenSocket]),
+    Mesg_convPID = spawn_link(knitter_tr_simple, server_mesg_conv, [self()]),
+    process_flag(trap_exit, true),
+    server(Control, Mesg_convPID, []).
 
 
-send_message(Connection, Process, Mesg) ->
-    Process ! {self(), toString, Mesg},
+send_message(Connection, Mesg_conv, Mesg) ->
+    Mesg_conv ! {self(), toString, Mesg},
     receive
 	{string, StrMesg} ->
-	    Connection ! {self(), sendMessage, StrMesg}
+	    Connection ! {self(), sendMessage, StrMesg},
+	    Mesg_conv;
+	{'EXIT', Mesg_conv, _} ->
+	    %%Hubo un error al convertir el mensaje. Hay que notificarlo
+	    spawn_link(knitter_tr_simple, server_mesg_conv, [self()])
     end.
 
 
-server(Control, Process, Connections) ->
+server(Control, Mesg_conv, Connections) ->
     receive
 %-------------------- ADD A NEW CONNECTION TO LIST
 	{newConection, Socket, ConnectionPID} ->
 	    {ok, AddressPort} = inet:peername (Socket),
-	    server(Control, Process, [{AddressPort, ConnectionPID} | Connections]);
+	    link(ConnectionPID),
+	    server(Control, Mesg_conv, [{AddressPort, ConnectionPID} | Connections]);
 %-------------------- SEND A MESSAGE TO PEER AGENT
 	{Control, sendMessage, Mesg} ->
 	    {ok, Receiver} = knitter_mesg:get_param(Mesg, receiver),
 	    case lists:keysearch(Receiver, 3, Connections) of
 		{value, {_, ConnectionPID, Receiver}} ->
-		    send_message(ConnectionPID, Process, Mesg),
-		    server(Control, Process, Connections);
+		    New_mesg_conv = send_message(ConnectionPID, Mesg_conv, Mesg),
+		    server(Control, New_mesg_conv, Connections);
 		false ->
 		    {ans_ok, Info} = knitter_ans:get_info(Receiver),
 		    {value, {address, Address}} = lists:keysearch(address, 1, Info),
@@ -59,26 +65,26 @@ server(Control, Process, Connections) ->
 			    New_connections = [{{Address, Port}, Connection, Receiver} | Connections],
 			    Connection
 		    end,
-		    send_message(Connection, Process, Mesg),
-		    server(Control, Process, New_connections)
+		    New_mesg_conv = send_message(Connection, Mesg_conv, Mesg),
+		    server(Control, New_mesg_conv, New_connections)
 	    end;
 %-------------------- WE HAVE RECEIVED A MESSAGE FROM AN AGENT
 	{Connection, receiveMessage, Mesg} ->
-	    Process ! {self(), toKQML, Mesg},
+	    Mesg_conv ! {self(), toKQML, Mesg},
 	    receive
 		{kqml, KQMLMesg} ->
 		    Control ! {self(), receiveMessage, KQMLMesg}
 	    end,
-	    server(Control, Process, Connections);
+	    server(Control, Mesg_conv, Connections);
 %-------------------- IGNORE ANY OTHER MESSAGES
 	_ ->
-	    server(Control, Process, Connections)
+	    server(Control, Mesg_conv, Connections)
     end.
 
 
 make_connection(Server, Address, Port) ->
     {ok, Socket} = gen_tcp:connect(Address, Port, []),
-    ControlPID = spawn(knitter_tr_simple, attend, [Server, Socket]),
+    ControlPID = spawn_link(knitter_tr_simple, attend, [Server, Socket]),
     ok = gen_tcp:controlling_process(Socket, ControlPID),
     ControlPID.
 
@@ -104,14 +110,14 @@ attend(Server, Socket) ->
     end.
 
 
-processMesg(Server) ->
+server_mesg_conv(Server) ->
     receive
 	{Server, toString, Mesg} ->
 	    Server ! {string, knitter_mesg_conv:kqml_to_string(Mesg)},
-	    processMesg(Server);
+	    server_mesg_conv(Server);
 	{Server, toKQML, Mesg} ->
 	    Server ! {kqml, knitter_mesg_conv:string_to_kqml(Mesg)},
-	    processMesg(Server);
+	    server_mesg_conv(Server);
 	_ ->
-	    processMesg(Server)
+	    server_mesg_conv(Server)
     end.
