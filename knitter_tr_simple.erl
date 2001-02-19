ef0467e396c49c2f715b63d21268f3a4df16101b
Juan A. Suarez Romero <jsuarezr@udc.es>
2001-02-19 16:59:58 +0000

    - Cambiado el nombre de "supervisor" a "server". - Ahora los datos se cogen del ANS. No necesitamos el fichero de configuración.
    git-svn-id: file:///var/svn/repos/knitter/trunk@38 55580ce4-9f14-0410-99c4-eacaa80e0a6d

--- knitter_tr_simple.erl
+++ knitter_tr_simple.erl
@@ -4,25 +4,25 @@
 -vsn('$Revision$ ').
 
 -export([start/2]).
--export([supervisor/2, listenIncoming/2, processMesg/1, attend/2]).
+-export([server/2, listenIncoming/2, processMesg/1, attend/2]).
 
 
 
 start(Control, Agent) ->
     case knitter_ans:get_info(Agent) of
 	{ans_ok, Info} ->
-	    spawn(knitter_tr_simple, supervisor, [Control, Info]);
+	    spawn(knitter_tr_simple, server, [Control, Info]);
 	{ans_error, _} ->
 	    exit("start/1: unable to start protocol")
     end.
 
 
-supervisor(Control, Options) ->
+server(Control, Options) ->
     {value, {port, Port}} = lists:keysearch(port, 1, Options),
     {ok, ListenSocket} = gen_tcp:listen(Port, []),
     spawn(knitter_tr_simple, listenIncoming, [self(), ListenSocket]),
     ProcessPID = spawn(knitter_tr_simple, processMesg, [self()]),
-    supervisor(Control, ProcessPID, []).
+    server(Control, ProcessPID, []).
 
 
 send_message(Connection, Process, Mesg) ->
@@ -33,19 +33,19 @@ send_message(Connection, Process, Mesg) ->
     end.
 
 
-supervisor(Control, Process, Connections) ->
+server(Control, Process, Connections) ->
     receive
 %-------------------- ADD A NEW CONNECTION TO LIST
 	{newConection, Socket, ConnectionPID} ->
 	    {ok, AddressPort} = inet:peername (Socket),
-	    supervisor(Control, Process, [{AddressPort, ConnectionPID} | Connections]);
+	    server(Control, Process, [{AddressPort, ConnectionPID} | Connections]);
 %-------------------- SEND A MESSAGE TO PEER AGENT
 	{Control, sendMessage, Mesg} ->
 	    {ok, Receiver} = knitter_util:get_param(Mesg, receiver),
 	    case lists:keysearch(Receiver, 3, Connections) of
 		{value, {_, ConnectionPID, Receiver}} ->
 		    send_message(ConnectionPID, Process, Mesg),
-		    supervisor(Control, Process, Connections);
+		    server(Control, Process, Connections);
 		false ->
 		    {ans_ok, Info} = knitter_ans:get_info(Receiver),
 		    {value, {address, Address}} = lists:keysearch(address, 1, Info),
@@ -60,7 +60,7 @@ supervisor(Control, Process, Connections) ->
 			    Connection
 		    end,
 		    send_message(Connection, Process, Mesg),
-		    supervisor(Control, Process, New_connections)
+		    server(Control, Process, New_connections)
 	    end;
 %-------------------- WE HAVE RECEIVED A MESSAGE FROM AN AGENT
 	{Connection, receiveMessage, Mesg} ->
@@ -69,49 +69,49 @@ supervisor(Control, Process, Connections) ->
 		{kqml, KQMLMesg} ->
 		    Control ! {self(), receiveMessage, KQMLMesg}
 	    end,
-	    supervisor(Control, Process, Connections);
+	    server(Control, Process, Connections);
 %-------------------- IGNORE ANY OTHER MESSAGES
 	_ ->
-	    supervisor(Control, Process, Connections)
+	    server(Control, Process, Connections)
     end.
 
 
-make_connection(Supervisor, Address, Port) ->
+make_connection(Server, Address, Port) ->
     {ok, Socket} = gen_tcp:connect(Address, Port, []),
-    ControlPID = spawn(knitter_tr_simple, attend, [Supervisor, Socket]),
+    ControlPID = spawn(knitter_tr_simple, attend, [Server, Socket]),
     ok = gen_tcp:controlling_process(Socket, ControlPID),
     ControlPID.
 
 
-listenIncoming(Supervisor, LSocket) ->
+listenIncoming(Server, LSocket) ->
     {ok, NewSocket} = gen_tcp:accept(LSocket),
-    ControlPID = spawn(knitter_tr_simple, attend, [Supervisor, NewSocket]),
-    Supervisor ! {newConection, NewSocket, ControlPID},
+    ControlPID = spawn(knitter_tr_simple, attend, [Server, NewSocket]),
+    Server ! {newConection, NewSocket, ControlPID},
     ok = gen_tcp:controlling_process(NewSocket, ControlPID),
-    listenIncoming(Supervisor, LSocket).
+    listenIncoming(Server, LSocket).
 
 
-attend(Supervisor, Socket) ->
+attend(Server, Socket) ->
     receive
 	{tcp, Socket, Data} ->
-	    Supervisor ! {self(), receiveMessage, Data},
-	    attend(Supervisor, Socket);
-	{Supervisor, sendMessage, Mesg} ->
+	    Server ! {self(), receiveMessage, Data},
+	    attend(Server, Socket);
+	{Server, sendMessage, Mesg} ->
 	    ok = gen_tcp:send(Socket, Mesg),
-	    attend(Supervisor, Socket);
+	    attend(Server, Socket);
 	_ ->
-	    attend(Supervisor, Socket)
+	    attend(Server, Socket)
     end.
 
 
-processMesg(Supervisor) ->
+processMesg(Server) ->
     receive
-	{Supervisor, toString, Mesg} ->
-	    Supervisor ! {string, knitter_mesg:toString(Mesg)},
-	    processMesg(Supervisor);
-	{Supervisor, toKQML, Mesg} ->
-	    Supervisor ! {kqml, knitter_mesg:toKQML(Mesg)},
-	    processMesg(Supervisor);
+	{Server, toString, Mesg} ->
+	    Server ! {string, knitter_mesg:toString(Mesg)},
+	    processMesg(Server);
+	{Server, toKQML, Mesg} ->
+	    Server ! {kqml, knitter_mesg:toKQML(Mesg)},
+	    processMesg(Server);
 	_ ->
-	    processMesg(Supervisor)
+	    processMesg(Server)
     end.
